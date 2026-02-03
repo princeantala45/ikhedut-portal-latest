3a1ebcfcf518876aeaaee20471d6a171f5a990bb
Prince <princeantala45@gmail.com>
2026-02-03 16:40:31 +0530

    Pass reset module changed

--- ikhedut_portal/settings.py
+++ ikhedut_portal/settings.py
@@ -165,8 +165,18 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "infoikhedutportal@gmail.com"
-EMAIL_HOST_PASSWORD = "fnrz lzsc tqwm xtdl"
+# EMAIL_HOST_USER = "infoikhedutportal@gmail.com"
+# EMAIL_HOST_PASSWORD = "fnrz lzsc tqwm xtdl"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+SITE_ID = 1
+
+PASSWORD_RESET_TIMEOUT = 3600 
+
+EMAIL_SUBJECT_PREFIX = "[Ikhedut Portal] "
+DEFAULT_DOMAIN = "ikhedut-portal-latest.onrender.com"
+
+
 
 DEFAULT_FROM_EMAIL = "Ikhedut Portal Support <infoikhedutportal@gmail.com>"
 
