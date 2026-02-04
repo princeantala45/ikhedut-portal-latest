3a56eab5f267caa85d52ce1c1f44f3acbfe6ec89
Prince <princeantala45@gmail.com>
2026-02-04 11:16:52 +0530

    make fixed header

--- ikhedut/serializer.py
+++ ikhedut/serializer.py
@@ -1,4 +1,3 @@
-
 from ikhedut.models import Ad, CropSale,Contact,Order, Signup
 from rest_framework import serializers
 from django.contrib.auth.models import User
--- ikhedut_portal/settings.py
+++ ikhedut_portal/settings.py
@@ -191,7 +191,7 @@ LOGOUT_REDIRECT_URL = "/login/"
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
